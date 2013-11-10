39c65d03df3f58f7a099e0d561128b5c2afa13b0
paulovelho <paulovelho@paulovelho.com>
2013-11-10 17:18:05 -0200

    ottvlc graddle fix

--- ottvlc/src/main/java/ottvlc/ottvlc.java
+++ ottvlc/src/main/java/ottvlc/ottvlc.java
@@ -1,4 +1,17 @@
 package ottvlc;
 
+import uk.co.caprica.vlcj.binding.LibVlc;
+import uk.co.caprica.vlcj.runtime.RuntimeUtil;
+
+import com.sun.jna.Native;
+import com.sun.jna.NativeLibrary;
+
+
 public class ottvlc {
+
+    public ottvlc(){
+        Native.loadLibrary(RuntimeUtil.getLibVlcLibraryName(), LibVlc.class);
+    }
+
+
 }
